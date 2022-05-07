18ec6d12a4f49d524924aef2a28fef35ff483ab2
Meir Sedlis <meirsedlis@gmail.com>
2022-05-07 15:27:51 -0500

    fix feelings for input validation

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,14 +1,27 @@
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom'
 
 function Feeling() {
+    const [feeling, setFeeling] = useState(0);
+    const history = useHistory();
+
+    const handleFeelings = () => {
+        //dispatch the feeling to the store
+        history.push('/understanding')
+    }
+
   return (
     <div>
       <h1>How are you feeling today?</h1>
       <label htmlFor="feeling">Feeling?</label>
-      <input type="number" name="feeling" />
-      <Link to="/understanding">
-        <button>NEXT</button>
-      </Link>
+      <input 
+        type="number" 
+        name="feeling" 
+        value={feeling}
+        onChange={(e)=> setFeeling(e.target.value)}/>
+        <button onClick={handleFeelings}>NEXT</button>
+    
     </div>
   );
 }
